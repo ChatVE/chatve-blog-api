95949eae8fd9d863ac91fc7b5688dfad827e0524
Oluwatobi Ekundayo <tobiekundayo07@gmail.com>
2023-08-20 09:58:22 +0100

    Finished Up

--- core/serializers.py
+++ core/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 from .models import *
 
 class CommentSerializer(serializers.ModelSerializer):
@@ -11,9 +12,14 @@ class CommentSerializer(serializers.ModelSerializer):
             'date_posted',
         ]
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username']
 
 class PostSerializer(serializers.ModelSerializer):
     comment = CommentSerializer(many=True, read_only=True)
+    author = UserSerializer()
     class Meta:
         model = Post
         fields = [
