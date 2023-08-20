130cf2c054e8cc52a9ee5902d6a37536001c8cac
Oluwatobi Ekundayo <tobiekundayo07@gmail.com>
2023-08-20 10:20:00 +0100

    Added Django SummerNote

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from .models import *
+from django_summernote.admin import SummernoteModelAdmin
 
-admin.site.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    # List of fields that should use the Summernote editor
+    summernote_fields = ('body',)
+
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(Comment)
 
 # Register your models here.
